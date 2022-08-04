f1e69d690c18f013700c1d7880f6c1243124270a
Nekxis <nexispolska16@gmail.com>
2022-08-05 01:03:49 +0200

    version fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-
+import React, {useState} from 'react';
 import './App.css';
 import Nav from "./components/nav/Nav";
 import Article from "./components/article/Article";
+import AdvancedStatistics from "./components/advanced-statistics/Advanced-Statistics";
+
+
 
 function App() {
+    const {route ,setRoute} = useState('home');
   return (
     <div>
-        <Nav />
+        <Nav isLogin ={route} />
         <Article />
+        <AdvancedStatistics />
     </div>
   );
 }
---
+++ src/components/advanced-statistics/Advanced-Statistics.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Advanced-Statistic.css'
+
+function AdvancedStatistics() {
+    return (
+        <div className="AdvancedStatistics">
+            <div className="input">
+            {/*input*/}
+            {/*tablica inputu*/}
+            </div>
+            <div className="text-statistics">
+                <h4 className="title-statistics">Advanced Statistics</h4>
+                <p className="description-statistics">Trac how your links are performing across our advanced statistics dashboard.</p>
+            </div>
+            <div className="cards"></div>
+        </div>
+    )
+}
+
+export default AdvancedStatistics;
