7dbe56688da2975ede5738919f0ca19dcf3e9127
Nekxis <nexispolska16@gmail.com>
2022-08-22 22:20:35 +0200

    card ready

--- src/components/advanced-statistics/Advanced-Statistics.js
+++ src/components/advanced-statistics/Advanced-Statistics.js
@@ -9,21 +9,36 @@ import Picture3 from "./theImg/icon-fully-customizable.svg"
 
 function AdvancedStatistics() {
     return (
-        <div className="AdvancedStatistics flex flex-col justify-center h-192 w-full p-8 box-border">
+        <div className="AdvancedStatistics flex flex-col justify-center h-256 w-full p-8 box-border">
             <div className="input block">
             {/*input*/}
             {/*tablica inputu*/}
             </div>
-            <div className="flex justify-start flex-col p-0">
+            <div className="flex justify-start  flex-col px-56 py-10">
                 <h4 className="title-statistics text-3xl flex self-center m-0">Advanced Statistics</h4>
-                <p className="description-statistics text-xl flex self-center m-0 w-3/5 text-center leading-6">Trac how your links are performing across our advanced statistics dashboard.</p>
+                <p className="description-statistics text-xl leading-relaxed flex self-center m-0 w-3/5 text-center leading-6">Trac how your links are performing across our advanced statistics dashboard.</p>
             </div>
-            <div className="cards flex relative justify-center h-1/2 p-10">
-                <Card image={Picture1}/>
+            <div className="cards flex relative justify-center h-2/5 px-5 py-12">
+                <Card
+                    align={`self-start`}
+                    image={Picture1}
+                    title={`Brand Recognition`}
+                    major={`Boost your brand recognition witch each click. Generic link don't mean a thing. Branded links help instill confidence in your content.`}
+                />
                 <Separator />
-                <Card image={Picture2}/>
+                <Card
+                    align={`self-center`}
+                    image={Picture2}
+                    title={`Detailed records`}
+                    major={`Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.`}
+                />
                 <Separator />
-                <Card image={Picture3}/>
+                <Card
+                    align={`self-end`}
+                    image={Picture3}
+                    title={`Fully Customizable`}
+                    major={`Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.`}
+                />
             </div>
         </div>
     )
--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,14 +1,14 @@
 import React from "react";
-import './Card.css'
 
-function Card({image}) {
+function Card({image, align, title, major}) {
     return (
-        <div className={'w-1/4 h-4/5 bg-white self-center justify-around relative p-5 rounded-md flex flex-col'}>
+
+        <div className={`w-1/4 h-5/6 bg-white justify-around relative  p-5 pt-12 rounded-md flex flex-col ${align}`} >
             <div className={'w-20 h-20 bg-the-purple rounded-full absolute flex justify-center -top-10'}>
                 <img className={'self-center flex'} src={image} alt="test"/>
             </div>
-            <p className={'text-xl flex  self-center'}>Title</p>
-            <p className={'text-the-light-grey flex self-center'}>Major text</p>
+            <p className={'text-xl flex  self-start'}>{title}</p>
+            <p className={'text-the-light-grey flex self-center'}>{major}</p>
         </div>
     )
 }
